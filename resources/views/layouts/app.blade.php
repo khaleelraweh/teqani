e89f37a2882ab79529aac54e72a9e10b0e9bca69
khaleel raweh <khaleelvisa@gmail.com>
2024-10-20 16:58:48 +0300

    save all

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="Ecommerce project">
+    <meta name="description" content="المعهد التقني العالى للعلوم الطبية">
     <meta name="robots" content="all,follow">
 
     <!-- CSRF Token -->
