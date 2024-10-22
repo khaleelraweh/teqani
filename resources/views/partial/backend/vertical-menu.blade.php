c913c493afc73592df620ce1de9208d16e1dd50d
khaleel raweh <khaleelvisa@gmail.com>
2024-10-23 00:52:33 +0300

    save all

--- resources/views/partial/backend/vertical-menu.blade.php
+++ resources/views/partial/backend/vertical-menu.blade.php
@@ -1,5 +1,5 @@
 @php
-    // get the current RouteName Fired this page 
+    // get the current RouteName Fired this page
     $current_page = \Route::currentRouteName();
 @endphp
 
@@ -11,11 +11,23 @@
         <!-- User details -->
         <div class="user-profile text-center mt-3">
             <div class="">
-                <img src="{{asset('backend/images/users/avatar-1.jpg')}}" alt="" class="avatar-md rounded-circle">
+                @php
+                    if (auth()->user()->user_image != null) {
+                        $user_img = asset('assets/users/' . auth()->user()->user_image);
+
+                        if (!file_exists(public_path('assets/users/' . auth()->user()->user_image))) {
+                            $user_img = asset('image/not_found/avator2.webp');
+                        }
+                    } else {
+                        $user_img = asset('image/not_found/avator2.webp');
+                    }
+                @endphp
+                <img src="{{ $user_img }}" alt="" class="avatar-md rounded-circle">
             </div>
             <div class="mt-3">
-                <h4 class="font-size-16 mb-1">{{auth()->user()->full_name}}</h4>
-                <span class="text-muted"><i class="ri-record-circle-line align-middle font-size-14 text-success"></i> Online</span>
+                <h4 class="font-size-16 mb-1">{{ auth()->user()->full_name }}</h4>
+                <span class="text-muted"><i class="ri-record-circle-line align-middle font-size-14 text-success"></i>
+                    Online</span>
             </div>
         </div>
 
@@ -23,34 +35,34 @@
         <div id="sidebar-menu">
             <!-- Left Menu Start -->
             <ul class="metismenu list-unstyled" id="side-menu">
-                
+
                 <li class="menu-title">
                     Menu
                 </li>
 
                 @foreach ($admin_side_menu as $menu)
-                    @if ( count($menu->appearedChildren) == 0)
+                    @if (count($menu->appearedChildren) == 0)
                         <li>
-                            <a href="{{route('admin.'.$menu->as)}}" class="waves-effect">
-                                <i class="{{$menu->icon != null ? $menu->icon : 'fas fa-home'}}"></i>
-                                <span>{{$menu->display_name}}</span>
+                            <a href="{{ route('admin.' . $menu->as) }}" class="waves-effect">
+                                <i class="{{ $menu->icon != null ? $menu->icon : 'fas fa-home' }}"></i>
+                                <span>{{ $menu->display_name }}</span>
                             </a>
                         </li>
                     @else
                         {{-- sup menu title --}}
                         <li>
                             <a href="javascript: void(0);" class="has-arrow waves-effect">
-                                <i class="{{$menu->icon != null ? $menu->icon : 'fas fa-home'}}"></i>
-                                <span>{{$menu->display_name}}</span>
+                                <i class="{{ $menu->icon != null ? $menu->icon : 'fas fa-home' }}"></i>
+                                <span>{{ $menu->display_name }}</span>
                             </a>
                             {{-- sup menu item  --}}
-                            @if ($menu->appearedChildren !== null && count($menu->appearedChildren) > 0 )
+                            @if ($menu->appearedChildren !== null && count($menu->appearedChildren) > 0)
                                 <ul class="sub-menu" aria-expanded="false">
                                     @foreach ($menu->appearedChildren as $sub_menu)
                                         <li>
-                                            <a href="{{route('admin.' . $sub_menu->as)}}">
-                                                <i class="{{$menu->icon != null ? $menu->icon : 'fas fa-home'}}"></i>
-                                                <span> {{$sub_menu->display_name}} </span>
+                                            <a href="{{ route('admin.' . $sub_menu->as) }}">
+                                                <i class="{{ $menu->icon != null ? $menu->icon : 'fas fa-home' }}"></i>
+                                                <span> {{ $sub_menu->display_name }} </span>
                                             </a>
                                         </li>
                                     @endforeach
