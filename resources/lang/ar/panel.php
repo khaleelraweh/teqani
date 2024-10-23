bdb16be4bc890e38857791975e72fa3daed6da19
khaleel raweh <khaleelvisa@gmail.com>
2024-10-23 15:15:11 +0300

    save all

--- resources/lang/ar/panel.php
+++ resources/lang/ar/panel.php
@@ -313,7 +313,7 @@ return [
     'manage_document_archive'                               =>  'إدارة ارشيف الوثائق',
     'show_document_archives'                               => 'إرشيف الوثائق',
     'add_new_document_archive'                             => 'إرشيف مستند جديد',
-    'edit_existing_document_archive'                       => 'تعديل بيانات مستند جديد',
+    'edit_existing_document_archive'                       => 'تعديل بيانات مستند موجود',
 
 
     // ........... commons in Archive   ....... //
